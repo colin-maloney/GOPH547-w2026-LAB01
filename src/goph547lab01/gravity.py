05bdd3ccd327e18b26b01ef6826d9e21f26096fd
Colin.Maloney <colin.maloney@ucalgary.ca>
2026-02-02 09:20:35 -0700

    added code for gravity.py

---
+++ src/goph547lab01/gravity.py
@@ -0,0 +1,39 @@
+import numpy as np 
+
+def gravity_potential_point(x, xm, m, G=6.674e-11): 
+    """ compute gravity potential due to a point masss 
+    
+    Parameters 
+    x : array_like, shape(3, ) => coordinates of survey point
+    xm : array_like, shape(3, ) => coordinates of mass anommaly
+    m : float => mass of anommaly
+    G : gravitational constant
+    
+    Retruns 
+    gravity potential at x due to annomally at xm 
+    """ 
+    x = np.array(x, dtype=float.flatten()) 
+    xm = np.array(xm, dtype=float.flatten()) 
+    m = float(m) 
+    G = float(G) 
+    r = np.linalg.norm(x - xm) 
+    return G * m/r 
+
+def gravity_effect_point(x, xm, m , G=6.674e-11): 
+    """ compute gravity effect due to a point masss 
+    
+    Parameters 
+    x : array_like, shape(3, ) => coordinates of survey point
+    xm : array_like, shape(3, ) => coordinates of mass anommaly
+    m : float => mass of anommaly
+    G : gravitational constant
+    
+    Retruns 
+    gravity effect at x due to annomally at xm 
+    """  
+    x = np.array(x, dtype=float.flatten()) 
+    xm = np.array(xm, dtype=float.flatten()) 
+    m = float(m) 
+    G = float(G) 
+    r = np.linalg.norm(x - xm) 
+    return G * m * (x[2]-xm[2])/ r**3 
